cff16e88fa869e3840cf847bd3512acee6b716fe
Ivan Aksionau <ivanionov2015@mail.ru>
2023-05-24 19:26:38 +0300

    - update

--- src/test/java/com/ivan/aksionau/controller/BaseTest.java
+++ src/test/java/com/ivan/aksionau/controller/BaseTest.java
@@ -2,11 +2,14 @@ package com.ivan.aksionau.controller;
 
 import io.restassured.RestAssured;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 @SpringBootTest(classes = BaseTest.class)
-public class BaseTest {
+//@EnableAutoConfiguration
+//@SpringBootTest(classes = BaseTest.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class  BaseTest {
 
     @Value("${app.url}")
     protected String appUrl;
