d291eb4ef909e8c6f7a69fddc6d4e59eace3bd94
Ivan Aksionau <ivanionov2015@mail.ru>
2023-06-27 21:14:27 +0300

    - update

--- src/main/java/com/ivan/aksionau/springBootRestAPI/SpringBootRestApiApplication.java
+++ src/main/java/com/ivan/aksionau/springBootRestAPI/SpringBootRestApiApplication.java
@@ -2,13 +2,11 @@ package com.ivan.aksionau.springBootRestAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootRestApiApplication  {
+public class SpringBootRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
 }
